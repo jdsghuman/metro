7424633ece24c540d0f91babc63ba75ed516d07c
JD Ghuman <jdshguman@gmail.com>
2021-10-21 08:21:41 -0500

    Update import

--- src/pages/_app.jsx
+++ src/pages/_app.jsx
@@ -2,8 +2,8 @@ import React from 'react'
 import Router from 'next/router'
 import NProgress from 'nprogress'
 import Layout from '../components/layout'
-import '@styles/main.scss'
 import '@styles/nprogress.css'
+import '@styles/main.scss'
 
 Router.events.on('routeChangeStart', NProgress.start)
 Router.events.on('routeChangeError', NProgress.done)
